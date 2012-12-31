f8146e483d139ef41abad6b64834be2a34c3f7c0
thewebguy <kevinkhandjian@gmail.com>
2012-12-31 02:04:17 -0500

    test

--- request-test.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-
-request('http://instagr.am/p/TwbkxhHmdJ/media?s=t', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(response.request.uri);
-  } else {
-  	console.log(response.statusCode, error);
-  }
-})
